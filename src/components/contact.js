94c91f87264ce8ff4ef8de0f5eaf12b3f02a7713
Tella Boluwatife <boluwatifetella@gmail.com>
2022-11-05 20:26:06 +0100

    Router Done

---
+++ src/components/contact.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react";
+import "../index.css";
+
+const Contact = () => {
+  return (
+    <React.Fragment>
+      <main>
+        <h1 className="">Contact Me</h1>
+        <p>Hi there, contact me to ask me about anything you have in mind.</p>
+
+        <form>
+          <div>
+            <div>
+              <label>First name</label>
+              <input />
+            </div>
+          </div>
+        </form>
+      </main>
+    </React.Fragment>
+  );
+};
+
+export default Contact;
